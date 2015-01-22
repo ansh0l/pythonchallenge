9391777635b4809332beb51d4c4331008c2a533c
ansh0l <anshl.goyl@gmail.com>
2015-01-22 21:23:01 +0530

    prob #03 solved

--- 03.prob.py
+++ 03.prob.py
@@ -8,7 +8,14 @@ is_small = lambda ch: ord("a") <= ord(ch) <= ord("z")
 pattern = (is_small, ) + (is_big, ) * 3 + (is_small, ) + (is_big, ) * 3 + (is_small, )
 
 matches = lambda pat, st: all(x(y) for x, y in zip(pat, st))
+result = []
 for i in range(len(text)):
     st = text[i:i+9]
-    if matches(pattern, st):
+    if len(st) == 9 and matches(pattern, st):
         print st[1:8]
+        result.append(st[4])
+print result
+print ''.join(result)
+
+#Hit url: http://www.pythonchallenge.com/pc/def/linkedlist.html
+#Next, hit url: http://www.pythonchallenge.com/pc/def/linkedlist.php
